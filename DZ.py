ef55dc523ee30458fd440daa17741349d6541215
Sonya <97834173+Super-sonic13@users.noreply.github.com>
2022-01-24 21:10:08 +0200

    Update DZ.py

--- DZ.py
+++ DZ.py
@@ -1,9 +1,16 @@
-x1 = int(input("x1: "))
-x2 = int(input("x2: "))
-print(x1 // x2, ".", x1 % x2)
+#Task 1
+x1 = int(input("Введите первое число: "))
+x2 = int(input("Введите второе число: "))
+print("Целочисленный результат:", x1 // x2, "Остаток:", x1 % x2)
 
-
-guest_list = lower(str(input("Enter guest list: ")))
-guest_name = lower(str(input("Enter guest name: ")))
-
-print(bool(guest_name in guest_list))
+#Task 2
+guest_list = str(input("Enter guest list: "))
+guest_list=guest_list.lower()
+guest_list=guest_list.translate({ord(','): None})
+guest_list=guest_list.translate({ord('.'): None})
+guest_name = str(input("Enter guest name: "))
+guest_name=guest_name.lower()
+if bool(guest_name in guest_list) == True:
+    print("You can pass!")
+else:
+    print("You can't pass!")
